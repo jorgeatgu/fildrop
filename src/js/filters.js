7dc1ff388ec33e45a8c71f3568205749654655cd
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2015-11-04 21:36:47 +0100

    refactor js

--- src/js/filters.js
+++ src/js/filters.js
@@ -3,11 +3,46 @@
 $(document).ready(function() {
 
     var image = $("img");
-
     var btnSaturate = $(".btn-saturate");
     var saturate = "saturate";
     var btnSaturotate = $(".btn-saturotate");
     var saturotate = "saturotate";
+    var btnRotamatrix = $(".btn-rotamatrix");
+    var rotamatrix = "rotamatrix";
+    var btnTablen = $(".btn-tablen");
+    var table = "tablen";
+    var btnMultitable = $(".btn-multitable");
+    var multitable = "multitable";
+    var btnDishue = $(".btn-dishue");
+    var dishue = "dishue";
+    var btnMatrix = $(".btn-matrix");
+    var matrix = "matrix";
+    var btnMatrix2 = $(".btn-matrix-dos");
+    var matrix2 = "matrix-dos";
+    var btnHueRotate = $(".btn-hueRotate");
+    var huerotate = "huerotate";
+    var btnLuminance = $(".btn-luminance");
+    var luminance = "luminance";
+    var btnDiscrete = $(".btn-discrete");
+    var discrete = "discrete";
+    var btnDiscrete2 = $(".btn-discrete-dos");
+    var discrete2 = "discrete-dos";
+    var btnTable = $(".btn-table");
+    var table = "table";
+    var btnTable2 = $(".btn-table-dos");
+    var table2 = "table-dos";
+    var btnIdentity = $(".btn-identity");
+    var identity = "identity";
+    var btnLinear = $(".btn-linear");
+    var linear = "linear";
+    var btnGamma = $(".btn-gamma");
+    var gamma = "gamma";
+    var btnTurbelence = $(".btn-turbulence");
+    var turbulence = "turbulence";
+    var btnSepia = $(".btn-sepia");
+    var sepia = "sepia";
+    var btnTurbuMap = $(".btn-turbu-map");
+    var turbumap = "turbu-map";
 
     function addFilter(button, image, filter) {
         $(button).click(function() {
@@ -17,74 +52,25 @@ $(document).ready(function() {
 
     addFilter(btnSaturate, image, saturate);
     addFilter(btnSaturotate, image, saturotate);
+    addFilter(btnRotamatrix, image, rotamatrix);
+    addFilter(btnTablen, image, tablen);
+    addFilter(btnMultitable, image, multitable);
+    addFilter(btnDishue, image, dishue);
+    addFilter(btnMatrix, image, matrix);
+    addFilter(btnMatrix2, image, matrix2);
+    addFilter(btnHueRotate, image, huerotate);
+    addFilter(btnLuminance, image, luminance);
+    addFilter(btnDiscrete, image, discrete);
+    addFilter(btnDiscrete2, image, discrete2);
+    addFilter(btnTable, image, table);
+    addFilter(btnTable2, image, table2);
+    addFilter(btnIdentity, image, identity);
+    addFilter(btnLinear, image, linear);
+    addFilter(btnGamma, image, gamma);
+    addFilter(btnTurbelence, image, turbulence);
+    addFilter(btnSepia, image, sepia);
+    addFilter(btnTurbuMap, image, turbumap);
 
-    $(".btn-rotamatrix").click(function() {
-        $("img").removeClass().addClass("rotamatrix");
-    });
-
-    $(".btn-tablen").click(function() {
-        $("img").removeClass().addClass("tablen");
-    });
-
-    $(".btn-multitable").click(function() {
-        $("img").removeClass().addClass("multitable");
-    });
-
-    $(".btn-dishue").click(function() {
-        $("img").removeClass().addClass("dishue");
-    });
-
-    $(".btn-matrix").click(function() {
-        $("img").removeClass().addClass("matrix");
-    });
-
-    $(".btn-matrix-dos").click(function() {
-        $("img").removeClass().addClass("matrix-dos");
-    });
-
-    $(".btn-hueRotate").click(function() {
-        $("img").removeClass().addClass("huerotate");
-    });
-
-    $(".btn-luminance").click(function() {
-        $("img").removeClass().addClass("luminance");
-    });
-
-    $(".btn-discrete").click(function() {
-        $("img").removeClass().addClass("discrete");
-    });
-
-    $(".btn-discrete-dos").click(function() {
-        $("img").removeClass().addClass("discrete-dos");
-    });
-
-    $(".btn-table").click(function() {
-        $("img").removeClass().addClass("table");
-    });
-
-    $(".btn-table-dos").click(function() {
-        $("img").removeClass().addClass("table-dos");
-    });
-
-    $(".btn-identity").click(function() {
-        $("img").removeClass().addClass("identity");
-    });
-
-    $(".btn-linear").click(function() {
-        $("img").removeClass().addClass("linear");
-    });
-
-    $(".btn-gamma").click(function() {
-        $("img").removeClass().addClass("gamma");
-    });
-
-    $(".btn-turbulence").click(function() {
-        $("img").removeClass().addClass("turbulence");
-    });
-
-    $(".btn-sepia").click(function() {
-        $("img").removeClass().addClass("sepia");
-    });
 
     $(".btn-turbu-map").click(function() {
         $("img").removeClass().addClass("turbu-map");
